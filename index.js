710e40401142aa6f18a5faec9e0a0c98c0266758
Glitch (pickled-loving-suit) <none>
2023-11-07 07:41:47 +0000

    🗃👩‍👩‍👦 Checkpoint ./index.js:62590089/438

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ app.use(express.json());
 
 app.use('/', express.static('generator'));
 
+const { Database } = require("quickmongo");
+const db = new Database("mongodb+srv://vivianffq:mongodbpassword@cluster0.pkugayl.mongodb.net/?retryWrites=true&w=majority");
+db.on("ready", () => {
+    console.log("Connected to the database");
+});
+db.connect();
+
 app.listen(3000, ()=> {
     console.log("listening at localhost:3000");
 })
